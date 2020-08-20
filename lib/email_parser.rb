30826eff8c57d9546c2bc020ec6b00feec4b77ce
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-20 02:04:12 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,13 +5,16 @@
 
 class EmailAddressParser
 
-  attr_accessor :email
+  attr_accessor :name, :csv_emails
 
-  def initialize(email)
-    @email = email
+  def initialize(csv_emails)
+    @csv_emails = csv_emails
   end
 
   def parse
-
+    csv_emails.split.collect do |address|
+      address.split(',')
+    end
+    .flatten.uniq
   end
 end
